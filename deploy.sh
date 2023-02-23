ecf74e48eddc508e2af8482f5a76de278d7def5c
Tomás Weigenast <tomaswegenast@gmail.com>
2023-02-23 18:59:56 -0300

    add GoReleaser

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. ./github_token.sh
+
+VERSION=$(cat VERSION)
+
+if [[ $1 == '--dry' ]]; then
+echo "Release version $VERSION"
+goreleaser release --snapshot --clean
+else
+git tag -a v$VERSION -m "Release version v$VERSION"
+git push origin v$VERSION
+goreleaser release
+fi
